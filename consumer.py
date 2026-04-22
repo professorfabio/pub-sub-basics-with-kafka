df595f9721872337a27c5aae9dc1ee602636d3e1
Fábio M. Costa <fmc@ufg.br>
2026-04-22 15:10:02 -0300

    Document Kafka consumer options in consumer.py
    Added comments to explain Kafka consumer options.

--- consumer.py
+++ consumer.py
@@ -2,7 +2,12 @@ from kafka import KafkaConsumer
 from const import *
 import sys
 
+# Create consumer: Option 1 -- only consume new events
 consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
+
+# Create consumer: Option 2 -- consume old events (uncomment to test -- and comment Option 1 above)
+#consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT], auto_offset_reset='earliest')
+
 try:
   topic = sys.argv[1]
 except:
